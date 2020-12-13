3ec630b895fbd64fb18bf7be03749ce20eaf30ed
qiqimonkey4300 <yueqi04@gmail.com>
2020-12-13 10:37:36 -0500

    private method sorting() added and tested

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -4,7 +4,38 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList() {
     super();
   }
-  public OrderedArrayList(startingCapacity) {
+
+  public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
+
+  private void sorting() {
+    int min = 0;
+    T temp = super.get(0);
+    int x = 0;
+    for (int i = 0; i < size(); i++) {
+      min = i;
+      for (int j = i + 1; j < size(); j++) {
+        if (super.get(j).compareTo(super.get(i)) < 0) {
+          min = j;
+          x = 1;
+        }
+      }
+      temp = super.get(i);
+      set (i, super.get(min));
+      super.set(min, temp);
+      if (x != 0) {
+        i--; x = 0;
+      }
+    }
+  }
+
+  //public boolean add(T value) {
+  //}
+
+  //public void add(int index, T value) {
+  //}
+
+  //public T set(int index, T value) {
+  //}
 }
